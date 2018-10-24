f4c54182e5478e1ab83cda32f90cf8d80e620e87
Taufik Purnomosidi <taufik.purnomosidi@gmail.com>
2018-10-24 17:35:01 +0700

    Update Example: getMediaByTag, adding without credentials

--- examples/getMediasByTag.php
+++ examples/getMediasByTag.php
@@ -1,21 +1,31 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-$instagram = \IGScraper\Instagram::withCredentials('username', 'password', '/path/to/cache/folder');
-$instagram->login();
+/**
+ * Example with credentials
+ * 
+ */
+//$instagram = \IGScraper\Instagram::withCredentials('username', 'password', '/path/to/cache/folder');
+//$instagram->login();
 
-$medias = $instagram->getMediasByTag('youneverknow', 20);
+/**
+ * Example without credentials
+ * 
+ */
+$instagram = new \IGScraper\Instagram();
+
+$medias = $instagram->getMediasByTag('halodoccetaku', 20);
 $media = $medias[0];
-echo "Media info:\n";
+echo "MEDIA INFO:\n";
 echo "Id: {$media->getId()}\n";
 echo "Shortcode: {$media->getShortCode()}\n";
 echo "Created at: {$media->getCreatedTime()}\n";
 echo "Caption: {$media->getCaption()}\n";
-echo "Number of comments: {$media->getCommentsCount()}";
-echo "Number of likes: {$media->getLikesCount()}";
-echo "Get link: {$media->getLink()}";
-echo "High resolution image: {$media->getImageHighResolutionUrl()}";
-echo "Media type (video or image): {$media->getType()}";
+echo "Number of comments: {$media->getCommentsCount()}\n";
+echo "Number of likes: {$media->getLikesCount()}\n";
+echo "Get link: {$media->getLink()}\n";
+echo "High resolution image: {$media->getImageHighResolutionUrl()}\n";
+echo "Media type (video or image): {$media->getType()}\n";
 $account = $media->getOwner();
 echo "Account info:\n";
 echo "Id: {$account->getId()}\n";
